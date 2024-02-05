7524253655519eac50293e728f4fcec91fb4c1a7
ytyaru <yttry0@gmail.com>
2024-02-05 15:41:09 +0900

    途中

--- docs/0/js/main.js
+++ docs/0/js/main.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOMContentLoaded!!');
     const {h1, p, a} = van.tags
     const author = 'ytyaru'
+    /*
     Scene.set('main', div(h1('メイン画面'),p('これはメイン画面です。')))
     Scene.set('sub', div(h1('サブ画面'),p('これはサブ画面です。')))
     van.add(document.querySelector('main'), 
@@ -10,6 +11,24 @@ window.addEventListener('DOMContentLoaded', (event) => {
 //        p('Create a screen transition library.'),
     )
     van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make())
+    */
+    const tsv = `画面ID	UI名	型	ラベル	プレースホルダー	初期値	attrs	datalist
+scene-0	title	text	タイトル	表題	初期値		
+scene-0	category	select	カテゴリ		label-2		{"value":"label-1", "groupValue":{"value":"label-2"}}
+scene-0	created	date	作成日				
+scene-1	title	text	タイトル				
+scene-1	body	area	本文	プレースホルダー。\\n二行目。	初期値。\\n二行目。		
+scene-1	sex	radio	性別			{"male":"男", "female":"女"}
+scene-1	isMan	check	人間か		true		
+scene-1	viewer	div				{"tabindex":0, "contenteditable":true}	
+
+`
+    const uiEl = new UiEl()
+    const uiMap = uiEl.fromTsv(tsv, true)
+//    for (let [k,v] of uiMap) {
+//        van.add(document.body, div(h1(k), uiEl.makeTable(v)))
+//    }
+    van.add(document.body, uiEl.makeTables(uiMap))
 });
 window.addEventListener('beforeunload', (event) => {
     console.log('beforeunload!!');
--- docs/0/js/scene.js
+++ docs/0/js/scene.js
@@ -1,6 +1,6 @@
 (function(){
 class Scene extends Map {
-    constructor() { this._selected = null; this._displays = new Map(); }
+    constructor(iterable) { super(iterable); this._selected = null; this._displays = new Map(); }
     json(k) {
         const v = this.get(k)
         if (Type.isArray(v)) {
@@ -9,7 +9,7 @@ class Scene extends Map {
                 
             }
         }
-        if (Type.isElement(v))
+        //if (Type.isElement(v))
     }
     select(k) {
         if (!this.has(k)) { return }
@@ -31,34 +31,131 @@ class Scene extends Map {
     #show(k) { this[k].style.setProperty('display', this._displays.get(k)) }
 }
 class UiEl {
-    fromCsv() {
-
+    constructor() {
+        this._scene = new Map()
     }
-    loadCsv() {
-
+    async loadTsv(url) {
+        const res = await fetch(url)
+        const txt = await res.text()
+        return this.fromTsv(txt)
     }
-    make(name, options, innerText) {
-        const [el, op] = this.#elOp(name)
-        options = {...op, ...options}
-        return van.tags[el](options, innerText)
+    /*
+    fromCsv(text) {
+        const lines = text.split(/\r?\n/)
     }
-    #elOp(name) {
-        const el = this.#makeNameIsType(name)
+    #fromLines(lines, delimiter='\n') { // 画面ID,UIName,型,label,placeholder,value,attrs,datalist
+        const [sid, name, type, label, placeholder, value, attrs, datalist] = lines.split(delimiter)
+        if (!attrs) { attrs = {} }
+        attrs.id = `${sid.Chain}-${name.Chain}`
+        attrs.name = name.Camel
+        if (datalist) { attrs.list = `${attrs.id}-list` }
+        if (this._scenes.has(sid)) { this._scenes.set(sid, new Map()) }
+        if (this._scenes.get(sid).has(name)) { this._scenes.get(sid).set(name, {el:this.make(name, attrs, value), dl:this.#makeDatalist(attrs.list, datalist)) }
+    }
+    */
+    fromTsv(text, isHeaderTrim) {
+        const scenes = new Map()
+        text = text.trimLine()
+        text = ((isHeaderTrim) ? this.#removeHeader(text) : text)
+        console.log(text)
+        const lines = text.split(/\r?\n/)
+        console.log(lines)
+        for (let line of text.split(/\r?\n/)) { this.#fromLine(scenes, line) }
+        return scenes
+    }
+    #removeHeader(text) { const i=text.indexOf('\n'); return ((-1===i) ? text : text.substr(i+1)); }
+    #fromLine(scenes, line, delimiter='\t') { // 画面ID,UIName,型,label,placeholder,value,attrs,datalist
+        const [sid, name, type, label, placeholder, value, _attrs, _datalist] = line.split(delimiter)
+        console.log(sid, name, type, label, placeholder, value, _attrs, _datalist)
+        console.log(`_attrs:${_attrs}`, ((_attrs) ? 'YES' : 'NO'))
+        console.log(`value:${value}`)
+        const attrs = ((_attrs) ? JSON.parse(_attrs) : ({}))
+        const datalist = ((_datalist) ? JSON.parse(_datalist) : null)
+//        if (!attrs) { attrs = {} }
+        attrs.id = `${sid.Chain}-${name.Chain}`
+        attrs.name = name.Camel
+        attrs.placeholder = placeholder.replace('\\n', '\n')
+        if (!this.#isTextareaOrContenteditable(type, attrs) && 'select'!==type) { attrs.value = value }
+//        attrs.value = value
+        if (datalist) { attrs.list = `${attrs.id}-list` }
+        if (!scenes.has(sid)) { scenes.set(sid, new Map()) }
+        console.log(scenes, scenes.get(sid))
+        if (!scenes.get(sid).has(name)) { scenes.get(sid).set(name, ({el:this.#makeUi(type, value, attrs, value, datalist), dl:this.#makeDatalist(attrs.list, type, datalist), label:this.#makeLabel(attrs.id, label)})) }
+        return scenes
+    }
+    #makeUi(type, value, attrs, innerText, datalist) {
+        const [el, op] = this.#elOp(type)
+        console.log(el, {...op, ...attrs}, innerText.replace('\\n', '\n'), this.#setNewline(innerText, type, attrs))
+        // \\n -> <br> or &#13;
+ //       return van.tags[el]({...op, ...attrs}, this.#setNewline(innerText, type, attrs), this.#makeSelectOptions(el, datalist))
+        //return van.tags[el]({...op, ...options}, innerText.replace('\\n', ((this.#isTextareaOrContenteditable(type, attrs)) ? '<br>' : '\n')), this.#makeSelectOptions(el, datalist))
+        //return van.tags[el]({...op, ...options}, innerText.replace('\\n', ((this.#isTextarea(type)) ? '<br>' : '\n')), this.#makeSelectOptions(el, datalist))
+        return van.tags[el]({...op, ...attrs}, innerText.replace('\\n', '\n'), this.#makeSelectOptions(el, value, datalist))
+    }
+    #setNewline(text, type, attrs) { return text.replace('\\n', ((this.#isTextareaOrContenteditable(type, attrs)) ? '<br>' : '\n')) }
+    #isTextareaOrContenteditable(type, attrs) {
+        if (['area','textarea'].some(v=>v===type)) { return true }
+        else if (attrs.hasOwnProperty('contenteditable')) { return attrs.contenteditable }
+        return false
+    }
+    //#isTextarea(type) { return ['area','textarea'].some(v=>v===type) } ;
+    #elOp(type) {
+        const el = this.#makeNameIsType(type)
         if (el) { return el }
-        switch (name) {
+        switch (type) {
             case 'textarea':
             case 'area':
                 return ['textarea', {}]
-            case 'select': return [name, {}]
+            case 'select': return [type, {}]
             case 'check': return ['input', {type:'checkbox'}]
             case 'datetime': return ['input', {type:'datetime-local'}]
-            default: return ['input', {type:'text'}]
+//            case 'div': return ['div', {contenteditable:true}]
+//            default: return ['input', {type:'text'}]
+            default: return [type, {}]
         }
     }
-    #makeNameIsType(name) {
-        if (['text','checkbox','color','date','datetime-local','email','file','hidden','month','number','password','range','search','tel','text','time','url','week'].same(v=>v===name)) { return ['input', {type:name}] }
+    #makeNameIsType(type) {
+        if (['text','checkbox','radio','color','date','datetime-local','email','file','hidden','month','number','password','range','search','tel','text','time','url','week'].some(v=>v===type)) { return ['input', {type:type}] }
         return null
     }
+    #makeDatalist(id, type, valueLabelObj) {
+        if (!valueLabelObj) { return null }
+        if (!['text','search','url','tel','email','number','month','week','date','time','datetime','datetime-local','range','color','password'].some(v=>v===type)) { return }
+        //const data = JSON.parse(valueLabelObj)
+        //return datalist(Array.from(Object.entries(data)).map(([k,v])=>van.tags.option({value:k}, v)))
+        return datalist(this.#makeOptions(valueLabelObj))
+    }
+    #makeSelectOptions(tagName, value, valueLabelObj) {
+        console.log('#makeSelectOptions')
+        if ('select'!==tagName) { return null }
+        console.log(valueLabelObj)
+        return this.#makeOptions(valueLabelObj, value)
+//        const data = JSON.parse(valueLabelObj)
+//        return Array.from(Object.entries(data)).map(([k,v])=>van.tags.option({value:k}, v))
+    }
+    #makeOptionGroup(label, valueLabelObj, value) { console.log(label, valueLabelObj);return van.tags.optgroup({label:label}, this.#makeOptions(valueLabelObj, value)) }
+    #makeOptions(valueLabelObj, value) { return Array.from(Object.entries(valueLabelObj)).map(([k,v])=>{console.log(k,v);return ((Type.isStr(v)) ? van.tags.option({value:k, selected:(k===value)}, v) : this.#makeOptionGroup(k, v, value))}) }
+    //#makeOptions(valueLabelObj) { console.log(valueLabelObj); return Array.from(Object.entries(valueLabelObj)).map(([k,v])=>{console.log(k,v);return ((Type.isStr(v)) ? van.tags.option({value:k}, v) : this.#makeOptionGroup(k, v))}) }
+    //#makeOptions(valueLabelObj) { return Array.from(Object.entries(JSON.parse(valueLabelObj))).map(([k,v])=>((Type.isStr(v)) ? van.tags.option({value:k}, v) : this.#makeOptionGroup(k, v))) }
+    //#makeOptions(valueLabelObj) { return Array.from(Object.entries(JSON.parse(valueLabelObj))).map(([k,v])=>van.tags.option({value:k}, v)) }
+    #makeLabel(id, text) { return van.tags.label({for:id}, text) }
+
+    makeTables(scenes) { return Array.from(scenes).map(([k,v])=>this.makeTable(v, k)) }
+    /*
+    makeTables(scenes) {
+        for (let [k,v] of uiMap) {
+            van.add(document.body, div(h1(k), uiEl.makeTable(v, k)))
+        }
+    }
+    */
+    makeTable(scene, k) {
+        const trs = []
+        for (let [k,v] of scene) {
+            trs.push(van.tags.tr(van.tags.th(v.label), van.tags.td(v.el, v.dl)))
+        }
+        return van.tags.table(((k) ? van.tags.caption(k) : null), trs)
+    }
 }
+window.UiEl = UiEl
 window.Scene = new Scene();
 })()
